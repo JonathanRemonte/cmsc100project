67c6d5abf4d623e7c98d2218d920de43f5c914dd
JonathanRemonte <jonathanremonte@gmail.com>
2023-01-03 03:31:31 +0000

    feat: add get many todo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import { general } from './services/general/index.js';
 import { createBlog } from './services/blogs/create-blog.js';
+import { getManyBlog } from './services/blogs/get-many-blog.js';
 
 const prefix = '/api';
 
@@ -12,6 +13,9 @@ export async function build () {
 
   // create blog
   fastify.post(`${prefix}/blog`, createBlog);
+
+  // get many blog
+  fastify.get(`${prefix}/blog`, getManyBlog);
 
   return fastify;
 }
