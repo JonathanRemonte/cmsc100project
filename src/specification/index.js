a7aea07cb754742eed771cc574b32bb7ddffd1ec
JonathanRemonte <jonathanremonte@gmail.com>
2023-01-04 15:56:13 +0000

    refactor: move the code towards specification as well as refactor paths

---
+++ src/specification/index.js
@@ -0,0 +1,52 @@
+import { paths } from './paths/index.js';
+
+export const specification = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Blog App',
+        version: '0.0.1'
+    },
+    paths,
+    components: {
+        schemas: {
+            BlogObject: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'string'
+                    },
+                    title: {
+                        type: 'string'
+                    },
+                    description: {
+                        type: 'string'
+                    },
+                    comments: {
+                        type: 'string'
+                    },
+                    createdDate: {
+                        type: 'number'
+                    },
+                    updatedDate: {
+                        type: 'number'
+                    }
+                }
+            },
+            BlogRequestRequiredObject: {
+                type: 'object',
+                properties: {
+                    title: {
+                        type: 'string'
+                    },
+                    description: {
+                        type: 'string'
+                    }
+                },
+                required: [
+                    'title',
+                    'description'
+                ]
+            }
+        }
+    }
+};
