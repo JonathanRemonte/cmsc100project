2493834e8f6e0aa0ec8771298be73716c32e2a9e
JonathanRemonte <jonathanremonte@gmail.com>
2023-01-11 09:31:00 +0000

    feat: modifying and setting up the blog list page

--- src/frontend/pages/page-blogs/template.js
+++ src/frontend/pages/page-blogs/template.js
@@ -5,17 +5,24 @@ export function template () {
   return html`
     <style>
       .blog {
-        display: flex;
-        align-items: center;
+        display: list-item;
+        align-items: left;
         padding: 12px;
       }
       .blog * {
         flex: 1;
       }
     </style>
-    <h2>
+
+    <h1>
+      Create New Blog
+    </h1>
+
+    <blog-component @submit-blog="${this.createBlog}"></blog-component>
+
+    <h1>
       Blogs
-    </h2>
+    </h1>
     ${this.errorMessage
       // if this is an errorMessage
       ? html`
@@ -27,15 +34,29 @@ export function template () {
     <div class="blog-list">
       ${this.blogs.map(blog => html`
         <div class="blog">
+          <h2> Entry: </h2>
+          <h3> User:
+            ${blog.username}
+          </h3>
+          <h3> Title: 
           <a href="/blogs/${blog.id}">
             ${blog.title}
-          </a>
-          <p>
-            ${blog.description} - Created at: ${new Date(blog.createdDate)}
-          </p>
-        </div>
+          </a></h3>
+          <h3> Body:
+            ${blog.description}
+          </h3>
+          <h3> Comments:
+            <a href="/blogs/${blog.id}">
+              Click here to see comments...
+          </a></h3>
+          <h3> Posted Date:
+            ${new Date(blog.createdDate)}
+          </h3>
+          <h3> Updated Date:
+            ${new Date(blog.updatedDate)}
+          </h3>  
       `)}
     </div>
-    <blog-component @submit-blog="${this.createBlog}"></blog-component>
+    
   `;
 }
